5a117e2d793360307405c2b7e608d57a64916669
emCsq <emcsq@live.ca>
2017-02-12 12:46:01 -0700

    Update proxy.c

--- proxy.c
+++ proxy.c
@@ -10,6 +10,8 @@
 #include <dirent.h>
 #include <pthread.h>
 #include <ctype.h>
+#include <netdb.h>
+
 
 /*
 *   Yue Chen                10065082    T03
@@ -44,86 +46,85 @@ void *ongoingsocket(void *params)
     int i;
     char hexbuffer[11]; //a buffer for holding 10 characters for the string part
     int N = themparams->autoN;
-
-    printf("");
-
+    int n;
     while(1){
 
         //get input from socket
-        recv(themparams->input, inputbuffer, 1024, 0);
+        n = read(themparams->input, inputbuffer, sizeof(inputbuffer));
+		if (n > 0) {
+			//print operation here
+			if(themparams->logOptions == 0){
 
+			}
+			else if (themparams->logOptions == 1) {   //-raw
+				printf("%s%s\n",themparams->direct, inputbuffer);
+			}
+			else if (themparams->logOptions == 2) { //-strip
+				
+				printf("%s", themparams->direct);
+				//loop to check if ascii
+				for(i = 0; inputbuffer[i] != '\0'; i++){
+					if(isascii(inputbuffer[i])){
+						printf("%c", inputbuffer[i]);
+					}else{
+						printf(".");
+					}
+				}
+			}
+			else if (themparams->logOptions == 3) {  //-hexdump -C
+				//not just print hex!!!!
+				//output is like ----hex---- string
+				//this will print 10 hex and 10 string on each line
+				printf("%s", themparams->direct);
+				for (i = 0; inputbuffer[i] != '\0'; i++) {
+					printf("%02x", inputbuffer[i]);
+					//store the print character in hexbuffer
+					//only 10 at a time
+					if(isascii(inputbuffer[i])){
+						hexbuffer[i%10] = inputbuffer[i];
+					}else{
+						hexbuffer[i%10] = '.';
+					}
+					if((i%10) == 9){
+						hexbuffer[10] = '\0';
+						printf(" %s\n%s", hexbuffer,themparams->direct);
+						bzero(hexbuffer, sizeof(hexbuffer));
+					}
+				}
+			}
+			else if (themparams->logOptions == 4) {  //-autoN 
+				printf("%s", themparams->direct);
+				for(i = 0; inputbuffer[i] != '\0'; i++)
+				{
+					if(i%themparams->autoN == 0){
+						printf("\n%s", themparams->direct);
+					}
+					if(inputbuffer[i] == '\\'){
+						printf("\\\\");
+					}
+					else if(inputbuffer[i] == '\t'){
+						printf("\\t");
+					}
+					else if(inputbuffer[i] == '\n'){
+						printf("\\n");
+					}
+					else if(inputbuffer[i] == '\r'){
+						printf("\\r");
+					}
+					else if(inputbuffer[i] < 32 && inputbuffer[i] > 127){
+						printf("%c", inputbuffer[i]);
+					}else{
+						printf("\\%c%02x", inputbuffer[i], inputbuffer[i]);
+					}
+				}
+			}
 
-        //print operation here
-        if(themparams->logOptions == 0){
-
-        }
-        else if (themparams->logOptions == 1) {   //-raw
-            printf("%s%s\n",themparams->direct, inputbuffer);
-        }
-        else if (themparams->logOptions == 2) { //-strip
-            
-            printf("%s", themparams->direct);
-            //loop to check if ascii
-            for(i = 0; inputbuffer[i] != '\0'; i++){
-                if(isascii(inputbuffer[i])){
-                    printf("%c", inputbuffer[i]);
-                }else{
-                    printf(".");
-                }
-            }
-        }
-        else if (themparams->logOptions == 3) {  //-hexdump -C
-            //not just print hex!!!!
-            //output is like ----hex---- string
-            //this will print 10 hex and 10 string on each line
-            printf("%s", themparams->direct);
-            for (i = 0; inputbuffer[i] != '\0'; i++) {
-                printf("%02x", inputbuffer[i]);
-                //store the print character in hexbuffer
-                //only 10 at a time
-                if(isascii(inputbuffer[i])){
-                    hexbuffer[i%10] = inputbuffer[i];
-                }else{
-                    hexbuffer[i%10] = '.';
-                }
-                if((i%10) == 9){
-                    hexbuffer[10] = '\0';
-                    printf(" %s\n%s", hexbuffer,themparams->direct);
-                    bzero(hexbuffer, sizeof(hexbuffer));
-                }
-            }
-        }
-        else if (themparams->logOptions == 4) {  //-autoN 
-            printf("%s", themparams->direct);
-            for(i = 0; inputbuffer[i] != '\0'; i++)
-            {
-                if(i%themparams->autoN == 0){
-                    printf("\n%s", themparams->direct);
-                }
-                if(inputbuffer[i] == '\\'){
-                    printf("\\\\");
-                }
-                else if(inputbuffer[i] == '\t'){
-                    printf("\\t");
-                }
-                else if(inputbuffer[i] == '\n'){
-                    printf("\\n");
-                }
-                else if(inputbuffer[i] == '\r'){
-                    printf("\\r");
-                }
-                else if(inputbuffer[i] < 32 && inputbuffer[i] > 127){
-                    printf("%c", inputbuffer[i]);
-                }else{
-                    printf("\\%c%02x", inputbuffer[i], inputbuffer[i]);
-                }
-            }
-        }
-
-        bzero(inputbuffer, sizeof(inputbuffer));
-
-        //send to output socket
-        write(themparams->output, inputbuffer, sizeof(inputbuffer));
+			//send to output socket
+			printf("inputbuffer contains %s\n", inputbuffer);
+			write(themparams->output, inputbuffer, n);
+			bzero(inputbuffer, sizeof(inputbuffer));
+		}
+        
     }
 }
 
@@ -147,6 +148,7 @@ int main(int argc, char *argv[])
     //socket variables
     struct sockaddr_in serv_addr, cli_addr;
     struct sockaddr_in serverAddr;
+    struct hostent *serverHost;
 
     //input socket variables
     int sockfd, newsockfd, srcPort, sockcd;
@@ -162,30 +164,41 @@ int main(int argc, char *argv[])
     }
     else if(argc == 4){
         srcPort = atoi(argv[1]);
-        strcpy(server, argv[2]);
+        //strcpy(server, argv[2]);
         dstPort = atoi(argv[3]);
+        serverHost = gethostbyname(argv[2]);
+        printf("argv2 contains %s\n", argv[2]);
     }
     else if(argc == 5){
         strcpy(tempstr, argv[1]);
         srcPort = atoi(argv[2]);
-        strcpy(server, argv[3]);
+        //strcpy(server, argv[3]);
         dstPort = atoi(argv[4]);
-        if (strncmp(tempstr, "-raw ", 5) == 0) {
+        serverHost = gethostbyname(argv[3]);
+        printf("argv3 contains %s\n", argv[3]);
+        printf("serverHost contains %s\n", serverHost);
+        if(serverHost == NULL){
+			error("everything is wrong");
+		}
+        //printf("tempstr contains %s\n", tempstr);
+        if (strncmp(tempstr, "-raw", 4) == 0) {
             logOptions = 1;
+            //printf("I set logOptions to 1\n");
         }
-        else if (strncmp(tempstr, "-strip ", 7) == 0) {
+        else if (strncmp(tempstr, "-strip", 6) == 0) {
             logOptions = 2;
         }
-        else if (strncmp(tempstr, "-hex ", 5) == 0) {
+        else if (strncmp(tempstr, "-hex", 4) == 0) {
             logOptions = 3;
         }
-        else if (strncmp(tempstr, "-autoN ", 7) == 0) {
+        else if (strncmp(tempstr, "-autoN", 7) == 0) {
             logOptions = 4;
             char c = tempstr[6];
             autoN = atoi(&c);
         }
     }
 
+	printf("server string contains %s\n", server);
 
     //create our host server to wait for clients
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
@@ -200,12 +213,17 @@ int main(int argc, char *argv[])
         error("Cannot bind src port");
     }
     listen(sockfd, 5);
-    clilen = sizeof(cli_addr);
-
+    clilen - sizeof(cli_addr);
+	
+	
     //create the remote host
+    bzero((char *) &serverAddr, sizeof(serverAddr));
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_port = htons(dstPort);
-    serverAddr.sin_addr.s_addr = inet_addr(server);
+    //serverAddr.sin_addr.s_addr = inet_addr(serverHost->h_addr);
+    printf("serverAddr is %s\n", serverHost->h_addr);
+    printf("reach 223\n");
+    bcopy((char*)serverHost->h_addr, (char *)&serverAddr.sin_addr.s_addr, serverHost->h_length);
     memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
     serveraddr_size = sizeof(serverAddr);
 
@@ -214,15 +232,17 @@ int main(int argc, char *argv[])
         struct threadstuff params1;
         struct threadstuff params2;
         pthread_t thread1, thread2;
+		sockcd = socket(PF_INET, SOCK_STREAM, 0);
     while(1){
         newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-        printf("got new connection: %d", newsockfd);
         if(newsockfd < 0){
             error("error on accepting new client");
         }
         //create a new socket to communicate with server
-        sockcd = socket(PF_INET, SOCK_STREAM, 0);
-        connect(sockcd, (struct sockaddr *) &serverAddr, serveraddr_size);
+        
+        if (connect(sockcd, (struct sockaddr*) &serverAddr, serveraddr_size) < 0) {
+			error("ERROR, server-side socket failed.\n");
+		}
         //create thread params
         params1.input = newsockfd;
         params1.output = sockcd;
